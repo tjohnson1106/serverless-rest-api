e2d4f00fd967664ba0740f63bb6719949e36e0a3
Thomas_Johnson <tjohnson1106@gmail.com>
2018-02-23 20:47:12 -0500

    added permissions/resources to yaml file

--- handler.js
+++ handler.js
@@ -1,12 +1,17 @@
-'use strict';
+"use strict";
+
+const uuid = require("uuid");
+const AWS = require("aws-sdk");
+
+const dynamoDb = new AWS.DynamoDb.DocumentClient({});
 
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: event
+    })
   };
 
   callback(null, response);
